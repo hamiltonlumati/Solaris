d13cf17919bfec127019c1f61f40da8faa2cb85e
Hamilton Lumati <hamiltonlumati@gmail.com>
2023-10-30 10:37:43 +0100

    Edição do Título das Notificações

--- resources/views/notificacoes.blade.php
+++ resources/views/notificacoes.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-            {{ __('Dados dos  últimos 30 dias') }}
+            {{ __('Notificações') }}
         </h2>
     </x-slot>
 
